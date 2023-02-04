c77c14081a4c93e1d6ca92ab95331bef9a1aca67
shadowdevnotreal <43219706+shadowdevnotreal@users.noreply.github.com>
2023-02-04 03:26:06 -0500

    Update wttb.sh

--- Program Files/wttb.sh
+++ Program Files/wttb.sh
@@ -4,12 +4,12 @@
 echo -e "\033[1;31m_  _ _  _ _ _  _ ____ ____ ____ ____ _     _  _ ____ _  _ _  _\033[0m"
 echo -e "\033[1;31m|  | |\ | | |  | |___ |__/ [__  |__| |     |\/| |___ |\ | |  | \033[0m"
 echo -e "\033[1;31m|__| | \| |  \/  |___ |  \ ___] |  | |___  |  | |___ | \| |__| \033[0m" 
-echo -e "\033[1;33m                       Another Wishmaster Production \033[0m"
+echo -e "\033[1;33m                      Another Wishmaster Production \033[0m"
 echo -e "\033[1;34m     Let me give you a sample to get you in the spirit of the game! \033[0m"
 
-echo -e "\033[1;35m                       Someone give me a bottle of rum! \033[0m"
+echo -e "\033[1;35m                    Someone give me a bottle of Vodka! \033[0m"
 
-echo -e "\033[1;36m                                Here we go! \033[0m"
+echo -e "\033[1;36m                              Here we go! \033[0m"
 
 
 function run_script1 {
